575d5d2ecc914c9818f9a548ee07ef6c31a1afc7
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2022-05-19 22:07:23 +0500

    verifying user

--- AL-Arz Dictionary/controller/authconfig.js
+++ AL-Arz Dictionary/controller/authconfig.js
@@ -25,3 +25,16 @@ exports.signup = (req, res) => {
     user: "mediumtutorial2021@gmail.com", 
     pass: "medium2021t", 
   };
+  module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
+    console.log("Check");
+    transport.sendMail({
+      from: user,
+      to: email,
+      subject: "Please confirm your account",
+      html: `<h1>Email Confirmation</h1>
+          <h2>Hello ${name}</h2>
+          <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+          <a href=http://localhost:8081/confirm/${confirmationCode}> Click here</a>
+          </div>`,
+    }).catch(err => console.log(err));
+  };
