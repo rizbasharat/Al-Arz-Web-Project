19f1c73a8d32df6d98f5254b57af3b58cf7a3098
Muhammad Ahmad <f190906@nu.edu.pk>
2022-05-20 06:32:00 +0500

    db

---
+++ .history/AL-Arz Dictionary/controller/server_20220520062948.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Al Arz." });
+});
